f657dac3f6f5be20c2685b09e0b01e72f2e69fa4
mauro <mnaranjososa@gmail.com>
2025-04-29 03:45:14 -0300

    funcion para transferir nodos realizada

--- ejercicio2.c
+++ ejercicio2.c
@@ -17,14 +17,15 @@ typedef struct Nodo
 }Nodo;
 
 Nodo * CrearListaVacia();
-Nodo * CrearNodo(char *buff, int duracion, int ID);
-void InsertarNodo(Nodo ** start, Nodo * Nodo);
+Nodo * CrearNodo (char *buff, int duracion, int ID);
+void InsertarNodo (Nodo ** start, Nodo * Nodo);
+Nodo * QuitarNodo (Nodo ** start, int ID);
 
 int main ()
 {
     char buff[50];
     char opcion;
-    int duracion, ID = 999;
+    int duracion, ID = 1000, idBuscado;
     Nodo * listaPendientes = CrearListaVacia();
     Nodo * listaRealizadas = CrearListaVacia();
     
@@ -36,12 +37,26 @@ int main ()
         printf("ingrese la duracion de la tarea entre 10 y 100: ");
         scanf("%d", &duracion);
         Nodo * nuevoNodo = CrearNodo(buff, duracion, ID);
+        ID++;
         InsertarNodo(&listaPendientes, nuevoNodo);
         
         fflush(stdin);
         printf("\nDesea agregar otra tarea? (s/n): ");
         scanf("%c", &opcion);
     } while (opcion == 's' || opcion == 'S');
+
+    do {
+        printf("ingrese el ID de la tarea que fue realizada: ");
+        scanf("%d", &idBuscado);
+        Nodo * nodoQuitado = QuitarNodo(&listaPendientes, ID);
+        InsertarNodo(&listaRealizadas, nodoQuitado);
+        
+        fflush(stdin);
+        printf("\nDesea pasar otra tarea realizada? (s/n): ");
+        scanf("%c", &opcion);
+    } while (opcion == 's' || opcion == 'S');
+
+    
     
     return 0;
 }
@@ -54,7 +69,7 @@ Nodo * CrearListaVacia()
 Nodo * CrearNodo(char *buff, int duracion ,int ID)
 {
     Nodo * nuevoNodo = (Nodo *) malloc(sizeof(Nodo));
-    nuevoNodo->T.TareaId = ID++;
+    nuevoNodo->T.TareaId = ID;
     nuevoNodo->T.Descripcion = (char *)malloc((strlen(buff)+1)*sizeof(char));
     strcpy(nuevoNodo->T.Descripcion, buff);
     
@@ -68,4 +83,21 @@ void InsertarNodo(Nodo ** start, Nodo * Nodo)
 {
     Nodo->Siguiente = *start;
     *start = Nodo;
+}
+
+Nodo * QuitarNodo (Nodo ** start, int ID)
+{
+    Nodo ** aux = start;
+    while (aux != NULL && (*aux)->T.TareaId != ID)
+    {
+        aux = &(*aux)->Siguiente;
+    }
+    if (*aux)
+    {
+        Nodo * temp = *aux;
+        *aux = (*aux)->Siguiente;
+        temp->Siguiente = NULL;
+        return temp;
+    }
+    return NULL;
 }
